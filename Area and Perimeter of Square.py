8792672a76ddcdeb19ab9fa64acaf32d9a207951
Kota Naga Venkata Teja <22a91a1275@aec.edu.in>
2024-12-27 15:44:57 +0530

    Uploading code: Area and Perimeter of Square.py

---
+++ Area and Perimeter of Square.py
@@ -0,0 +1,10 @@
+import java.util.*;
+public class teja{
+public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    System.out.print(a*a + " ");
+    System.out.print(a*4);
+
+}
+}
